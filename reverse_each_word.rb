e55a7980a12f95414eccc362d5b4bf7fa92931fa
benevolent-demo-1612 <tevinsherman@yahoo.com>
2019-08-06 22:54:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ end
 def def reverse_each_word(sentence1)
     new_arr = []
     arr = sentence1.split(" ")
-    arr.each { |i| new_arr << i.reverse }
+    arr.collect { |i| new_arr << i.reverse }
     new_arr.join(" ")
 end
 end
